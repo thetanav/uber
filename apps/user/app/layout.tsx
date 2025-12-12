df93c12870c4f79a97c783b4138b3e79e033cf6f
Tanav Poswal <tanavposwal939@gmail.com>
2025-12-12 23:56:54 +0530

    user shadcn install

--- apps/user/app/layout.tsx
+++ apps/user/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Providers from "../components/providers";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Providers>
+        <body>{children}</body>
+      </Providers>
     </html>
   );
 }
